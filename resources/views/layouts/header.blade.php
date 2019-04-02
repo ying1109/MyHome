9fbfc2beeddb7382f94b42871ece9259a234583f
ying1109 <44599811+ying1109@users.noreply.github.com>
2019-04-02 17:52:06 +0800

    commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,10 +3,10 @@
     <div id="left">
         <span id="title">后台管理</span>
     </div>
-    <i id="top_icon" class="fa fa-align-justify fa-2x"></i>
+    <a href="#"><i id="top_icon" class="fa fa-align-justify fa-2x"></i></a>
     <div id="right">
         <ul>
-            <li><span>nickname</span></li>
+            <li><span>account</span></li>
             <li>
                 <a href="">
                     <i class="fa fa-power-off fa-lg"></i>
